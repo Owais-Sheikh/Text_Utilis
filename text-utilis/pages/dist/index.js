3838f9a9c72ec45fbb28e5e8e9ec9856b4b478ba
Owais-Sheikh <owaisshk1501@gmail.com>
2023-04-11 01:29:28 +0500

    Create responsive navbar using tailwind css

---
+++ text-utilis/pages/dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+exports.__esModule = true;
+var google_1 = require("next/font/google");
+var inter = google_1.Inter({ subsets: ['latin'] });
+function Home() {
+    return (React.createElement("main", { className: "flex min-h-screen flex-col items-center justify-between p-24" }));
+}
+exports["default"] = Home;
